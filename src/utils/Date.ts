f256e6be6f1e4b7711bbf99cff4ece791c09933a
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-02-09 14:01:25 -0500

    .

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -1,4 +1,4 @@
-// Función para calcular la edad basándonos en el año actual (2026)
+
 export const calculateAge = (birthDate: string) => {
     if (!birthDate || birthDate.startsWith('0')) return 'N/A';
     const birthYear = new Date(birthDate).getFullYear();
