c5ad5ddef8393e6f834b558e987ef88c6ed6477b
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-02-09 22:33:32 -0500

    Prepare to deploy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:30000/api',
+    baseURL: 'https://ugliest-aleece-jorgeluis-d39b7ba0.koyeb.app/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
